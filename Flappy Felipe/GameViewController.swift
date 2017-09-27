19cb9478e13ae1b38086c63e3bf2f295c5802fea
Vina Melody <vina_melody@yahoo.com>
2017-09-27 14:18:14 +0800

    Clean up and set up
    Video 2

--- Flappy Felipe/GameViewController.swift
+++ Flappy Felipe/GameViewController.swift
@@ -11,44 +11,41 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
+        // 1. Setting the scene size right
+        if let skView = self.view as? SKView {
+            if skView.scene == nil {
+                let aspectRatio = skView.bounds.size.height / skView.bounds.size.width
+                let scene = GameScene(size: CGSize(width: 320, height: 320 * aspectRatio))
                 
-                // Present the scene
-                view.presentScene(scene)
+                skView.showsFPS = false
+                skView.showsNodeCount = false
+                skView.showsPhysics = false
+                skView.ignoresSiblingOrder = true
+                
+                scene.scaleMode = .aspectFit
+                skView.presentScene(scene)
             }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
         }
+        // 1. end
     }
-
+    
     override var shouldAutorotate: Bool {
         return true
     }
-
+    
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
+        return [.portrait]
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
-
+    
     override var prefersStatusBarHidden: Bool {
         return true
     }
