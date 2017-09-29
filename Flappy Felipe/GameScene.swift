589896c426a37e795cdd1d9d441edb29979455a0
Vina Melody <vina_melody@yahoo.com>
2017-09-29 16:28:39 +0800

    Showing background and foreground image
    Video 2

--- Flappy Felipe/GameScene.swift
+++ Flappy Felipe/GameScene.swift
@@ -9,10 +9,41 @@
 import SpriteKit
 import GameplayKit
 
+enum Layer: CGFloat {
+    case background
+    case foreground
+}
+
 class GameScene: SKScene {
     
+    let worldNode = SKNode()
+    var playableStart: CGFloat = 0
+    var playableHeight: CGFloat = 0
+    
     override func didMove(to view: SKView) {
+        addChild(worldNode)
+        setupBackground()
+        setupForeground()
+    }
+    
+    func setupBackground() {
+        let background = SKSpriteNode(imageNamed: "background.png")
+        background.anchorPoint = CGPoint(x: 0.5, y: 1.0)
+        background.position = CGPoint(x: size.width / 2, y: size.height)
+        background.zPosition = Layer.background.rawValue
         
+        playableStart = size.height - background.size.height
+        playableHeight = background.size.height
         
+        worldNode.addChild(background)
+    }
+    
+    func setupForeground() {
+        let foreground = SKSpriteNode(imageNamed: "Ground.png")
+        foreground.anchorPoint = CGPoint(x: 0.0, y: 1.0)
+        foreground.position = CGPoint(x: 0, y: playableStart)
+        foreground.zPosition = Layer.background.rawValue
+        
+        worldNode.addChild(foreground)
     }
 }
