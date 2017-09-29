5d5e0498ece59548a6f8b93ed729a9b63fb6edb0
Vina Melody <vina_melody@yahoo.com>
2017-09-29 16:52:15 +0800

    Move the foreground
    Video 3

--- Flappy Felipe/GameScene.swift
+++ Flappy Felipe/GameScene.swift
@@ -20,6 +20,12 @@ class GameScene: SKScene {
     var playableStart: CGFloat = 0
     var playableHeight: CGFloat = 0
     
+    let numberOfForegrounds = 2
+    let groundSpeed: CGFloat = 150
+    
+    var deltaTime: TimeInterval = 0
+    var lastUpdateTimeInterval: TimeInterval = 0
+    
     override func didMove(to view: SKView) {
         addChild(worldNode)
         setupBackground()
@@ -27,6 +33,7 @@ class GameScene: SKScene {
     }
     
     func setupBackground() {
+        
         let background = SKSpriteNode(imageNamed: "background.png")
         background.anchorPoint = CGPoint(x: 0.5, y: 1.0)
         background.position = CGPoint(x: size.width / 2, y: size.height)
@@ -39,11 +46,39 @@ class GameScene: SKScene {
     }
     
     func setupForeground() {
-        let foreground = SKSpriteNode(imageNamed: "Ground.png")
-        foreground.anchorPoint = CGPoint(x: 0.0, y: 1.0)
-        foreground.position = CGPoint(x: 0, y: playableStart)
-        foreground.zPosition = Layer.background.rawValue
         
-        worldNode.addChild(foreground)
+        for i in 0..<numberOfForegrounds {
+            let foreground = SKSpriteNode(imageNamed: "Ground.png")
+            foreground.anchorPoint = CGPoint(x: 0.0, y: 1.0)
+            foreground.position = CGPoint(x: CGFloat(i) * foreground.size.width, y: playableStart)
+            foreground.zPosition = Layer.background.rawValue
+            foreground.name = "foreground"
+            worldNode.addChild(foreground)
+        }
+        
+    }
+    
+    func updateForeground() {
+        worldNode.enumerateChildNodes(withName: "foreground") { (node, stop) in
+            if let foreground = node as? SKSpriteNode {
+                let moveAmount = CGPoint(x: -self.groundSpeed * CGFloat(self.deltaTime), y: 0)
+                foreground.position += moveAmount
+                
+                if foreground.position.x < -foreground.size.width {
+                    foreground.position += CGPoint(x: foreground.size.width * CGFloat(self.numberOfForegrounds), y: 0)
+                }
+            }
+        }
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        if lastUpdateTimeInterval == 0 {
+            lastUpdateTimeInterval = currentTime
+        }
+        
+        deltaTime = currentTime - lastUpdateTimeInterval
+        lastUpdateTimeInterval = currentTime
+        
+        updateForeground()
     }
 }
